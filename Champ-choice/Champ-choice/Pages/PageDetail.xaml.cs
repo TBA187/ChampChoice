2d992a2a5e10582ee955eefbeff2dc4026a9aba2
Sadri TBA <sadri_albaner@hotmail.com>
2018-05-14 22:21:23 +0200

    hey

---
+++ Champ-choice/Champ-choice/Pages/PageDetail.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Champ_choice.Model;
+using Champ_choice.Pages;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Champ_choice.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageDetail : ContentPage
+    {
+        public PageDetail()
+        {
+            InitializeComponent();
+
+            //using (var connection = new MySqlConnection("user id=alphajob_org;" +
+            //                 "password=Q1fshaXh0rgxhGj1n7;server=mysql49.unoeuro.com;" +
+            //                 "database=alphajob_org_db;" +
+            //                 "connection timeout=30"))
+            //{
+            //    connection.Open();
+            //    string sql = "SELECT * FROM Player";
+            //    using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+            //    {
+            //        using (var reader = cmd.ExecuteReader())
+            //        {
+            //            while (reader.Read())
+            //            {
+            //                //var p = new Player();
+            //                hey.Text = reader["playerName"].ToString();
+            //                //p.playerNr = (int)reader["playerNr"];
+            //                //p.position = reader["position"].ToString();
+            //                //listOfPlayers.Add(p);
+
+            //            }
+            //        }
+            //    }
+            //    connection.Close();
+            //}
+        }
+
+        //protected override void OnAppearing()
+        //{
+        //    //var listOfPlayers = new List<Player>();
+
+
+        //    //return listOfPlayers;
+        //}
+    }
+}
